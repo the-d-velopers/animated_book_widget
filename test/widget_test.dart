c093a24e1f00104bd3d6e82fd67974b69767747a
Vortex842 <nahuelat.8@gmail.com>
2024-01-25 00:20:07 -0300

    Use tests in main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,75 +1,8 @@
-import 'package:animated_book_list/src/widgets/widgets.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'test_cover_rect.dart';
+import 'test_findings_widgets.dart';
 
 void main() {
-  testAnimatedBookWidget();
+  testFindingWidgets();
 
-  testCoverWidget();
-
-  testContentWidget();
+  testCoverRect();
 }
-
-void testTo(
-  String info,
-  void Function(WidgetTester tester) expectedCallBack,
-) =>
-    testWidgets(
-      'Animated Book List: $info',
-      (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: AnimatedBookWidget(
-                  cover: ColoredBox(color: Colors.green),
-                  content: ColoredBox(color: Colors.red),
-                  size: Size(160, 200),
-                ),
-              ),
-            ),
-          ),
-        );
-
-        expectedCallBack(tester);
-      },
-    );
-
-void testAnimatedBookWidget() => testTo(
-      'Finding the Animated Book Widget',
-      (tester) {
-        final animatedBookWidget = find.byType(AnimatedBookWidget);
-
-        expect(animatedBookWidget, findsOneWidget);
-      },
-    );
-
-void testCoverWidget() => testTo(
-      'The Cover Widget is a descendant of Animated Book Widget',
-      (tester) {
-        final animatedBookWidget = find.byType(AnimatedBookWidget);
-        final coverWidget = find.byType(AnimatedCoverWidget);
-
-        final descendantCoverWidget = find.descendant(
-          of: animatedBookWidget,
-          matching: coverWidget,
-        );
-
-        expect(descendantCoverWidget, findsOneWidget);
-      },
-    );
-
-void testContentWidget() => testTo(
-      'The Content Widget is a descendant of Animated Book Widget',
-      (tester) {
-        final animatedBookWidget = find.byType(AnimatedBookWidget);
-        final coverWidget = find.byType(AnimatedContentWidget);
-
-        final descendantCoverWidget = find.descendant(
-          of: animatedBookWidget,
-          matching: coverWidget,
-        );
-
-        expect(descendantCoverWidget, findsOneWidget);
-      },
-    );
